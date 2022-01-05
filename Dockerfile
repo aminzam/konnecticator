44997241b3702f3776d8f136e8dc9a1ee41520b1
amin <aministrator2k@users.noreply.github.com>
2022-01-04 18:17:48 -0700

    Reorganized structure

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM openjdk:8
-COPY ./target/konnecticator-0.0.1.jar /usrl/local/bin/konnecticator.jar
-CMD ["java", "-jar", "/usrl/local/bin/konnecticator.jar"]
--- server/src/main/java/org/konnecticator/server/ServerApplication.java
+++ server/src/main/java/org/konnecticator/server/ServerApplication.java
@@ -1,13 +1,13 @@
-package org.konnecticator.konnecticator;
+package org.konnecticator.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KonnecticatorApplication {
+public class ServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KonnecticatorApplication.class, args);
+		SpringApplication.run(ServerApplication.class, args);
 	}
 
 }

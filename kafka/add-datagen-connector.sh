abddeccca4738a44dd7c2c539258a5e5da7ad16c
P1254E1 <amin.zamiri@gmail.com>
2022-07-05 10:27:47 -0600

    Add comments

--- kafka/add-datagen-connector.sh
+++ kafka/add-datagen-connector.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+
+# When connect cluster container is up it takes a while for the REST API to go live
 until $(curl --head --fail http://connect:8083/); do
     printf '...\n'
     sleep 3
 done
 
+# Call the REST endpoint with provided config to create a new connector
 curl -X POST -H "Content-Type: application/json" --data /etc/connector.config http://connect:8083/connectors
